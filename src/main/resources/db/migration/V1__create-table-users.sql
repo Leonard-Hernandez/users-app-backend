e12861dbd50a549cc9824cee168eb83427a0d12a
Leonard David Hernandez Parra <leonardhernandez02@gmail.com>
2024-11-25 22:12:19 -0500

    creando e schema y la table de sql y añadiendo 3 usuarios

---
+++ src/main/resources/db/migration/V1__create-table-users.sql
@@ -0,0 +1,9 @@
+create table users (
+    id int not null auto_increment,
+    name varchar(45),
+    last_name varchar(45),
+    email varchar(70) unique,
+    username varchar(45)  not null unique,
+    password varchar(60) not null,
+    primary key (id)
+);
---
+++ src/main/resources/db/migration/V2__insert-users-example.sql
@@ -0,0 +1,4 @@
+insert into users (name, last_name, email, username, password) values
+('leonard', 'hernandez', 'leonard@example.com', 'leonardUser', '123456'),
+('lubraska', 'ferreira', 'lubraska@example.com', 'lubraskaUser', '123456'),
+('andres', 'guzman', 'andres@example.com', 'andresUser', '123456');
