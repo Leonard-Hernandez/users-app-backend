66c59fb36f39cf4d2435d0d218305d77b19fcc2d
Leonard David Hernandez Parra <leonardhernandez02@gmail.com>
2024-11-30 00:45:50 -0500

    Agregando validaciones al backend

--- src/main/java/com/springboot/backend/userapp/users_app/controllers/UserController.java
+++ src/main/java/com/springboot/backend/userapp/users_app/controllers/UserController.java
@@ -2,13 +2,17 @@ package com.springboot.backend.userapp.users_app.controllers;
 
 import com.springboot.backend.userapp.users_app.entity.User;
 import com.springboot.backend.userapp.users_app.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @CrossOrigin(originPatterns = {"*"})
@@ -36,13 +40,19 @@ public class UserController {
 
     @PostMapping
     @Transactional
-    public  ResponseEntity<User> create(@RequestBody User user){
+    public  ResponseEntity<?> create(@Valid @RequestBody User user, BindingResult result){
+        if (result.hasErrors()){
+            return validation(result);
+        }
         user.setId(null);
         return ResponseEntity.status(HttpStatus.CREATED).body(this.userService.save(user));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user){
+    public ResponseEntity<?> update(@Valid @RequestBody User user, BindingResult result, @PathVariable Long id){
+        if (result.hasErrors()){
+            return validation(result);
+        }
         Optional<User> userOptional = this.userService.findById(id);
         if (userOptional.isPresent()){
             User dbUser = userOptional.get();
@@ -68,5 +78,14 @@ public class UserController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    private static ResponseEntity<Map<String, String>> validation(BindingResult result) {
+        Map<String, String> errors = new HashMap<>();
+        result.getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), "El campo " + error.getField() + " " + error.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
+
 
 }
--- src/main/java/com/springboot/backend/userapp/users_app/entity/User.java
+++ src/main/java/com/springboot/backend/userapp/users_app/entity/User.java
@@ -1,6 +1,10 @@
 package com.springboot.backend.userapp.users_app.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
@@ -10,12 +14,17 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String name;
-
+    @NotBlank
     private String lastname;
-
+    @NotEmpty
+    @Email
     private String email;
+    @NotBlank
+    @Size(min = 6, max = 12)
     private String username;
+    @NotBlank
     private String password;
 
     public Long getId() {
